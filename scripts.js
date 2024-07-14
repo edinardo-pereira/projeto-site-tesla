b9112a899bc924fb6da8fb9a4bac72d02975a457
edinardo-pereira <edinardopereyra@gmail.com>
2024-07-14 15:35:44 -0300

    Add files via upload
    Site Tesla

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+
+let formulario = document.querySelector(".formulario")
+
+
+
+
+function aparecerFormulario(){
+    formulario.style.left = "50%"
+    
+}
+
+function desaparecerFormulario(){
+    formulario.style.left = "-50%"
+    
+}
+
