4b3d92465433de1698c7e13014d157ac8e3155dd
edinardo-pereira <edinardopereyra@gmail.com>
2024-07-15 11:52:59 -0300

    Add files via upload
    Alterações no html e inclusão de novo formulario para test drive

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
 
+let formulario = document.querySelector(".formulario");
+let formulario2 = document.querySelector(".formulario2");
 
-
-let formulario = document.querySelector(".formulario")
-
-
-
-
-function aparecerFormulario(){
-    formulario.style.left = "50%"
-    
+function aparecerFormulario() {
+    formulario.style.left = "50%";
 }
 
-function desaparecerFormulario(){
-    formulario.style.left = "-50%"
-    
+function desaparecerFormulario() {
+    formulario.style.left = "-50%";
 }
 
+function aparecerTest() {
+    formulario2.style.left = "50%";
+}
+
+function desaparecerTest() {
+    formulario2.style.left = "-50%";
+    
+}
